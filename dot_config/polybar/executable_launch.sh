4fff67c115b4413b7adc621f1fb56e849cef690b
askrid <joonwoo3023@gmail.com>
2023-05-14 19:13:59 +0900

    update

--- dot_config/polybar/executable_launch.sh
+++ dot_config/polybar/executable_launch.sh
@@ -4,7 +4,9 @@
 killall -q polybar
 
 # Launch Polybar, using default config location ~/.config/polybar/config.ini
-polybar main 2>&1 | tee -a /tmp/polybar.log & disown
+polybar workspace 2>&1 | tee -a /tmp/polybar.log & disown
+polybar date 2>&1 | tee -a /tmp/polybar.log & disown
+polybar monitor 2>&1 | tee -a /tmp/polybar.log & disown
 
 echo "Polybar launched..."
 
