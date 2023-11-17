7ee3609808bc224f087e03c783d37b560d2090a1
askrid <joonwoo3023@gmail.com>
2023-11-18 01:32:18 +0900

    update

--- dot_config/nvim/after/plugin/treesitter.lua
+++ dot_config/nvim/after/plugin/treesitter.lua
@@ -1,10 +1,18 @@
 require'nvim-treesitter.configs'.setup {
-    ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "typescript", "go", "rust" },
-
+    ensure_installed = {
+        "c",
+        "cpp",
+        "go",
+        "rust",
+        "vim",
+        "vimdoc",
+        "lua",
+        "query",
+        "javascript",
+        "typescript",
+    },
     sync_install = false,
-
     auto_install = true,
-
     highlight = {
         enable = true,
    },
--- dot_config/nvim/lua/plugin.lua
+++ dot_config/nvim/lua/plugin.lua
@@ -1,17 +1,19 @@
-vim.cmd [[packadd packer.nvim]]
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
-return require('packer').startup(function(use)
-    use 'wbthomason/packer.nvim'
-
-    use {
-        'nvim-telescope/telescope.nvim',
-        tag = '0.1.3',
-        requires = { {'nvim-lua/plenary.nvim'} },
-    }
-
-    use {
-        'nvim-treesitter/nvim-treesitter',
-        { run = ':TSUpdate' },
-    }
-end)
+require("lazy").setup({
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim",
+    "nvim-treesitter/nvim-treesitter",
+})
 
