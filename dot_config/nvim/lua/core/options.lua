19b51e6725df48a7e146d7c737cf3923dcec3696
askrid <joonwoo3023@gmail.com>
2024-07-31 15:32:09 +0900

    update

--- dot_config/nvim/lua/core/options.lua
+++ dot_config/nvim/lua/core/options.lua
@@ -1,43 +1,26 @@
 vim.opt.clipboard = 'unnamedplus'
-
 vim.opt.guicursor = ''
-
 vim.opt.showmode = false
-
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.expandtab = true
-
 vim.opt.completeopt = { 'noinsert', 'menuone', 'noselect' }
-
 vim.opt.autoindent = true
-
 vim.opt.ttyfast = true
-
 vim.opt.wildmode = { 'longest', 'list' }
-
 vim.opt.incsearch = true
 vim.opt.hlsearch = true
-
 vim.opt.mouse = 'a'
 vim.opt.mouse = 'v'
-
 vim.opt.ignorecase = true
-
 vim.opt.showmatch = true
-
 vim.opt.shiftwidth = 4
-
 vim.opt.nu = true
 vim.opt.relativenumber = true
-
 vim.opt.swapfile = false
 vim.opt.backup = false
-
 vim.opt.wrap = false
-
 vim.opt.termguicolors = true
-
 vim.opt.scrolloff = 8
 vim.opt.isfname:append('@-@')
 
