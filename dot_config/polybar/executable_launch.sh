711fe90f8a010b5aa4fc23b192798e024841875a
askrid <joonwoo3023@gmail.com>
2025-12-03 02:34:47 +0900

    up my game

--- dot_config/polybar/executable_launch.sh
+++ dot_config/polybar/executable_launch.sh
@@ -4,9 +4,7 @@
 killall -q polybar
 
 # Launch Polybar, using default config location ~/.config/polybar/config.ini
-polybar workspace 2>&1 | tee -a /tmp/polybar.log & disown
-polybar date 2>&1 | tee -a /tmp/polybar.log & disown
-polybar monitor 2>&1 | tee -a /tmp/polybar.log & disown
+polybar uni 2>&1 | tee -a /tmp/polybar.log & disown
 
 echo "Polybar launched..."
 
