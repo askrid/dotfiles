6f94afdc65d68f44e5ef839bcf379c736820f384
askrid <joonwoo3023@gmail.com>
2026-03-30 04:12:54 +0900

    lf image preview

--- dot_config/lf/executable_preview.sh
+++ dot_config/lf/executable_preview.sh
@@ -1,8 +1,17 @@
 #!/bin/zsh
 
-case $(file --mime-type -Lb "$1") in
-    text/*)           bat --color=always --style=plain --paging=never "$1" ;;
-    application/pdf)  pdftotext "$1" - ;;
-    *)                file -b "$1" ;;
+file="$1"
+w="$2"
+h="$3"
+x="$4"
+y="$5"
+
+case $(file --mime-type -Lb "$file") in
+    text/*)           bat --color=always --style=plain --paging=never "$file" ;;
+    image/*)          kitten icat --stdin no --transfer-mode memory --place "${w}x${h}@${x}x${y}" "$file" < /dev/null > /dev/tty;;
+    application/pdf)  pdftotext "$file" - ;;
+    *)                file -b "$file" ;;
 esac
+
+exit 1 # do not cache
 
