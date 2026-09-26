2dfe34e6f2a80289244a19da58a9bae7e11ae560
askrid <joonwoo3023@gmail.com>
2026-09-26 14:32:17 +0900

    i can't see shit

--- dot_config/hypr/hyprland.lua
+++ dot_config/hypr/hyprland.lua
@@ -36,7 +36,7 @@ hl.config({
         rounding = 8,
         rounding_power = 10,
         active_opacity = 1.0,
-        inactive_opacity = 0.9,
+        inactive_opacity = 0.98,
         shadow = {
             enabled = true,
             range = 3,
